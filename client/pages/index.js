4c1c76b6ccd3e7891f6ea5e30d3bdba15d53f5a8
최현범 <chb1828@naver.com>
2022-02-19 12:13:32 +0900

    Spring server의 api를 사용해 화면에 나타낸다

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,49 @@
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
+import axios from "axios";
+import {useEffect, useState} from "react";
 
 export default function Home() {
 
-    const onChange = async (e) => {
+    const [apiResult,setApiResult] = useState("");
+    const [country, setCountry] = useState("krw");
+    const [result, setResult] = useState("");
 
+    const onChange = async e => {
+        setCountry(e.target.value);
+        setResult("");
     }
+
+    const onSubmit = e => {
+        e.preventDefault();
+        let number = e.target[0].value;
+        let value = calc(apiResult,number);
+        setResult(value);
+    }
+
+    const calc = (apiResult, number) => {
+        return (Number(apiResult) * Number(number)).toFixed(2);
+    }
+
+    //useEffect는 비동기적으로 동작
+    useEffect(() => {
+        let completed = false; //초기에는 실행해야 되기때문에 false flag 변수
+
+        //query를 리턴하는 함수를 result에 할당
+        async function get() {
+            const result = await axios.get(
+                `/api?param=${country}`
+            );
+            if (!completed) setApiResult(result.data);
+        }
+        get().catch(function(error) {
+            console.log(error);
+        });
+        return () => {
+            completed = true;
+        };
+
+    }, [country]);
+
 
   return (
       <div>
@@ -12,19 +51,21 @@ export default function Home() {
         <p>송금국가: 미국(USD)</p>
           <div>
               <span>수취국가: </span>
-              <select>
-                  <option>한국(KRW)</option>
-                  <option>일본(KRW)</option>
-                  <option>필리핀(KRW)</option>
+              <select onChange={onChange} value={country}>
+                  <option value="krw">한국(KRW)</option>
+                  <option value="jpy">일본(JPY)</option>
+                  <option value="php">필리핀(PHP)</option>
               </select>
           </div>
-        <p>환율: 1,119.93 KRW/USD</p>
+        <p>환율: {apiResult} {country}/USD</p>
         <div>
-            <form>
-                <span>송금액: </span><input type="number"/><span><strong>USD</strong></span><br/>
+            <form onSubmit={onSubmit}>
+                <span>송금액: </span><input type="number" name="number"/><span><strong>USD</strong></span><br/>
                 <button className={styles.submitBtn}><strong>Submit</strong></button>
             </form>
         </div>
+          <hr/>
+          <h3>{result ? `수취금액은 ${result} ${country.toUpperCase()} 입니다.` : ""}</h3>
       </div>
   )
 }
