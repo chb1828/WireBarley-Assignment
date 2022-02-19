b37740e3f51c4a4dcdc9ea6a9524914091549dbb
최현범 <chb1828@naver.com>
2022-02-19 09:03:49 +0900

    외부 api를 호출 하기 위한 webClient를 Bean으로 생성하고 apiKey를 properties로 분리한다

---
+++ server/src/main/java/com/chb/assignment/config/WebClientConfig.java
@@ -0,0 +1,25 @@
+package com.chb.assignment.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebClientConfig {
+
+    private final ApiProperties apiProperties;
+    private final String URL = "http://api.currencylayer.com/live";
+
+    @Bean
+    public WebClient webClient() {
+        WebClient webClient = WebClient.builder()
+                .baseUrl(URL+"?access_key="+apiProperties.getAccessKey())
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+        return webClient;
+    }
+}
